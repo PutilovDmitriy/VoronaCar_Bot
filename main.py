e704857572b833eecce3103e35bc1d044f105b2d
PutilovDmitriy <44982167+PutilovDmitriy@users.noreply.github.com>
2019-12-03 15:05:58 +0500

    Normal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import telebot
+#from telebot import apihelper
+#apihelper.proxy = {'https': 'socks5h://geek:socks@t.geekclass.ru:7777'}
 
 import constants
 import kb
@@ -320,7 +322,7 @@ def handle_text(message):
 
 # ELSE
     else:
-        bot.send_message(message.chat.id, 'fgfg')
+        bot.send_message(message.chat.id, 'Пожалуйста, используйте меню для доступа к моим функциям. Выберите интересующий вас пункт.')
 
 
 @bot.message_handler(content_types=['photo', 'document'])
