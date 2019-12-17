eac4658945c5f403f61561c7660ffce34bc4eca1
PutilovDmitriy <dmitrii.98.putilov@gmail.com>
2019-12-17 14:51:37 +0500

    db test 2

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ import re
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['postgres://yzluaullcfqgju:eed018f99ab7d25475295e67fa94e44a1dd7175b12cafc1171c65fae3788d0f2' \
-                          '@ec2-174-129-33-97.compute-1.amazonaws.com:5432/do9oe5f35bgi6']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 con = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = con.cursor()
